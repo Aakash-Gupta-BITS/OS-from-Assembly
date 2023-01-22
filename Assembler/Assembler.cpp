3e41a19ef3ad7cb32a9e091d12d8b88ff8ef731c
Aakash-Gupta-BITS <42832499+Aakash-Gupta-BITS@users.noreply.github.com>
2023-01-22 18:11:57 +0530

    Lexer Tested

--- Assembler/Assembler.cpp
+++ Assembler/Assembler.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "Lexer.h"
 
 using namespace std;
 
 int main()
 {
+    loadDFA();
+    Buffer bufer("demo_program.asm");
+
+    Token* t;
+    while ((t = getNextToken(bufer))->type != TokenType::TK_EOF)
+    {
+        cout << *t << endl;
+    }
+
     std::cout << "Hello World!\n";
 }
