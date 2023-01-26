5ab240ed088c213bebb98b99118d7d1e6162519b
Aakash-Gupta-BITS <42832499+Aakash-Gupta-BITS@users.noreply.github.com>
2023-01-27 01:15:32 +0530

    Added Parser for Lines

--- Assembler/Assembler.cpp
+++ Assembler/Assembler.cpp
@@ -3,16 +3,54 @@
 
 using namespace std;
 
+class Parser
+{
+    Buffer* buffer;
+    Token* last_token;
+
+public:
+    Parser(Buffer* buffer) : buffer{ std::move(buffer) }
+    {
+        this->last_token = nullptr;
+    }
+
+    vector<Token*> get_next_line()
+    {
+        if (last_token == nullptr)
+            last_token = getNextToken(*buffer);
+
+        if (last_token->type == TokenType::TK_EOF)
+            return { last_token };
+
+        vector<Token*> tokens{ last_token };
+        auto new_token = getNextToken(*buffer);
+        for (; new_token->line_number == last_token->line_number; new_token = getNextToken(*buffer))
+            tokens.push_back(new_token);
+
+        last_token = new_token;
+        return tokens;
+    }
+};
+
 int main()
 {
     loadDFA();
-    Buffer bufer("demo_program.asm");
+    Buffer* buffer = new Buffer("demo_program.asm");
+    Parser p(buffer);
 
-    Token* t;
-    while ((t = getNextToken(bufer))->type != TokenType::TK_EOF)
+    while (true)
     {
-        cout << *t << endl;
+        auto tokens = p.get_next_line();
+        if (tokens[0]->type == TokenType::TK_EOF)
+            break;
+
+        cout << "Line: " << tokens[0]->line_number << ": " << endl;
+        for (auto& x : tokens)
+        {
+            cout << "\t" << *x << endl;
+            delete x;
+        }
     }
 
-    std::cout << "Hello World!\n";
+    delete buffer;
 }
